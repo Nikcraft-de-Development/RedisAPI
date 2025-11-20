65f076e480e56beb72d20b53f053faacfecd621d
EinNxk <einnxk@gmail.com>
2025-11-20 18:52:14 +0100

    Fixes

--- src/main/java/de/einnik/redisapi/RedisAPI.java
+++ src/main/java/de/einnik/redisapi/RedisAPI.java
@@ -1,16 +1,10 @@
 package de.einnik.redisapi;
 
-import de.einnik.redisapi.api.handler.LanguageHandler;
 import de.einnik.redisapi.db.RedisCore;
-import de.einnik.redisapi.language.LanguageLoader;
 import de.einnik.redisapi.manager.ProfileManager;
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
-
-import java.io.Reader;
-import java.util.Locale;
 
 public final class RedisAPI extends JavaPlugin {
 
@@ -26,7 +20,6 @@ public final class RedisAPI extends JavaPlugin {
     @Override
     public void onEnable() {
         registerListeners();
-        Bukkit.getScheduler().runTaskAsynchronously(this, this::loadYaml);
     }
 
     @Override
@@ -41,7 +34,6 @@ public final class RedisAPI extends JavaPlugin {
 
     private void registerListeners(){
         Bukkit.getPluginManager().registerEvents(new ProfileManager(), this);
-        Bukkit.getPluginManager().registerEvents(new LanguageHandler(), this);
     }
 
     public static RedisCore getRedisCore(){
@@ -50,21 +42,5 @@ public final class RedisAPI extends JavaPlugin {
 
     public BukkitTask runTaskTimer(Runnable runnable, int delay, int period) {
         return Bukkit.getScheduler().runTaskTimer(this, runnable, delay, period);
-    }
-
-    public void loadYaml(){
-        Reader reader_de = getTextResource("german.yml");
-        if (reader_de == null){
-            Bukkit.getPluginManager().disablePlugin(this);
-            return;
-        }
-        LanguageLoader.languages.put(Locale.GERMAN, YamlConfiguration.loadConfiguration(reader_de));
-
-        Reader reader_en = getTextResource("en.yml");
-        if (reader_en == null){
-            Bukkit.getPluginManager().disablePlugin(this);
-            return;
-        }
-        LanguageLoader.languages.put(Locale.ENGLISH, YamlConfiguration.loadConfiguration(reader_en));
     }
 }
